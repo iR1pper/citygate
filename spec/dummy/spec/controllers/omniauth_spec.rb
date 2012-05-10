6da7ffe2140082863c51ced520bcf04d6001b145
Zamith <zamith.28@gmail.com>
2012-05-10 16:10:17 +0100

    Adding shared_examples

--- spec/dummy/spec/controllers/omniauth_spec.rb
+++ spec/dummy/spec/controllers/omniauth_spec.rb
@@ -2,79 +2,51 @@ require 'spec_helper'
 
 describe Users::OmniauthCallbacksController do
 
-  context "facebook" do
-    before (:each) do
-      request.env["omniauth.auth"] = OmniAuth.config.mock_auth[:facebook]
-      request.env["devise.mapping"] = Devise.mappings[:user]
-    end
-    
-    it "should create a new user" do
-      lambda {
-        get :facebook
-      }.should change(User,:count).by(1)
-    end
-    
-    it "should create a new authorization" do
-      lambda {
-        get :facebook
-      }.should change(Authorization,:count).by(1)
-    end
-    
-    context "not database" do
+  shared_examples "a provider for" do |provider|
+    describe "provider" do
       before (:each) do
-        get :facebook
+        request.env["omniauth.auth"] = OmniAuth.config.mock_auth[provider]
+        request.env["devise.mapping"] = Devise.mappings[:user]
       end
       
-      it "should create a authorization for facebook provider" do
-        assigns(:user).authorizations.last.provider.should == "Facebook"
+      it "should create a new user" do
+        lambda {
+          get provider
+        }.should change(User,:count).by(1)
       end
       
-      it "should create a authorization for the right user" do
-        assigns(:user).id.should == Authorization.last.user_id
-      end    
-      
-      it "should create a user with the correct email" do
-        assigns(:user).email.should == "zamith@groupbuddies.com"
-      end
-    end
-  end
-  
-  context "google" do
-    before (:each) do
-      request.env["omniauth.auth"] = OmniAuth.config.mock_auth[:google]
-      request.env["devise.mapping"] = Devise.mappings[:user]
-    end
-    
-    it "should create a new user" do
-      lambda {
-        get :google
-      }.should change(User,:count).by(1)
-    end
-    
-    it "should create a new authorization" do
-      lambda {
-        get :google
-      }.should change(Authorization,:count).by(1)
-    end
-    
-    context "not database" do
-      before (:each) do
-        get :google
+      it "should create a new authorization" do
+        lambda {
+          get provider
+        }.should change(Authorization,:count).by(1)
       end
       
-      it "should create a authorization for google provider" do
-        assigns(:user).authorizations.last.provider.should == "Google"
-      end
-      
-      it "should create a authorization for the right user" do
-        assigns(:user).id.should == Authorization.last.user_id
-      end    
-      
-      it "should create a user with the correct email" do
-        assigns(:user).email.should == "zamith@groupbuddies.com"
+      context "not database" do
+        before (:each) do
+          get provider
+        end
+        
+        it "should create a authorization for #{provider} provider" do
+          assigns(:user).authorizations.last.provider.should == provider.to_s.capitalize
+        end
+        
+        it "should create a authorization for the right user" do
+          assigns(:user).id.should == Authorization.last.user_id
+        end    
+        
+        it "should create a user with the correct email" do
+          assigns(:user).email.should == "zamith@groupbuddies.com"
+        end
       end
     end
   end
 
+  context "facebook" do
+    it_behaves_like "a provider for", :facebook
+  end
+  
+  context "google" do
+    it_behaves_like "a provider for", :google
+  end
 
 end
