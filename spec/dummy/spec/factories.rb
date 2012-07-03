3e9f52e43cdb1ee0b208f21f96ae48a0b0fae1d2
Pedro Pereira <pedro.mdp@gmail.com>
2012-07-03 19:52:54 +0100

    removed deprecated name field from rspec factory

--- spec/dummy/spec/factories.rb
+++ spec/dummy/spec/factories.rb
@@ -1,5 +1,5 @@
 Factory.define :user, :class => "Citygate::User" do |f|
-  f.name                  'Test User'
+  f.first_name            'User'
   f.email                 'example@example.com'
   f.password              'please'
   f.password_confirmation 'please'
@@ -11,7 +11,7 @@ end
 Factory.define :facebook_auth, :class => "Citygate::Authorization" do |f|
   f.provider   'Facebook'
   f.uid        '28'
-  f.name       'Testy McTest'
+  f.name       'User'
   f.link       'http://www.facebook.com/luis.zamith'
   f.image_url  'http://graph.facebook.com/754864768/picture?type=square'
 end
@@ -19,15 +19,15 @@ end
 Factory.define :google_auth, :class => "Citygate::Authorization" do |f|
   f.provider   'Google'
   f.uid        '28'
-  f.name       'Testy McTest'
+  f.name       'User'
 end
 
 Factory.define :facebook_user, :parent => :user do |user|
-  user.name nil
+  user.first_name nil
   user.after_create {|fb_user| Factory.create(:facebook_auth, :user => fb_user)}
 end
 
 Factory.define :google_user, :parent => :user do |user|
-  user.name nil
+  user.first_name nil
   user.after_create {|g_user| Factory.create(:google_auth, :user => g_user)}
 end
