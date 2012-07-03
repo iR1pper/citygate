363e12716749b175d37782f82cff01bfc5c553aa
Pedro Pereira <pedro.mdp@gmail.com>
2012-07-03 16:42:09 +0100

    cleaned user model spec

--- spec/dummy/spec/models/user_spec.rb
+++ spec/dummy/spec/models/user_spec.rb
@@ -1,35 +1,35 @@
 require 'spec_helper'
 
 describe Citygate::User do
-  
+
   before(:each) do
     @attr = { 
-      :name => "Example User",
-      :email => "user@example.com",
-      :password => "foobar",
+      :first_name            => "User",
+      :email                 => "user@example.com",
+      :password              => "foobar",
       :password_confirmation => "foobar"
     }
   end
-  
+
   it "should render the correct json hash" do
     fb_user = Factory.create(:facebook_user)
     fb_user.as_json.should == {
-      :email => "example@example.com",
-      :name  => "Testy McTest",
-      :link  => "http://www.facebook.com/luis.zamith",
-      :image => "http://graph.facebook.com/754864768/picture?type=square"
+      :first_name  => "User",
+      :email       => "example@example.com",
+      :link        => "http://www.facebook.com/luis.zamith",
+      :image       => "http://graph.facebook.com/754864768/picture?type=square"
     }
   end
-  
+
   it "should create a new instance given a valid attribute" do
     Citygate::User.create!(@attr)
   end
-  
+
   it "should require an email address" do
     no_email_user = Citygate::User.new(@attr.merge(:email => ""))
     no_email_user.should_not be_valid
   end
-  
+
   it "should accept valid email addresses" do
     addresses = %w[user@foo.com THE_USER@foo.bar.org first.last@foo.jp]
     addresses.each do |address|
@@ -37,7 +37,7 @@ describe Citygate::User do
       valid_email_user.should be_valid
     end
   end
-  
+
   it "should reject invalid email addresses" do
     addresses = %w[user@foo,com user_at_foo.org example.user@foo.]
     addresses.each do |address|
@@ -45,22 +45,21 @@ describe Citygate::User do
       invalid_email_user.should_not be_valid
     end
   end
-  
+
   it "should reject duplicate email addresses" do
     Citygate::User.create!(@attr)
     user_with_duplicate_email = Citygate::User.new(@attr)
     user_with_duplicate_email.should_not be_valid
   end
-  
+
   it "should reject email addresses identical up to case" do
     upcased_email = @attr[:email].upcase
     Citygate::User.create!(@attr.merge(:email => upcased_email))
     user_with_duplicate_email = Citygate::User.new(@attr)
     user_with_duplicate_email.should_not be_valid
   end
-  
-  describe "passwords" do
 
+  describe "passwords" do
     before(:each) do
       @user = Citygate::User.new(@attr)
     end
@@ -73,9 +72,8 @@ describe Citygate::User do
       @user.should respond_to(:password_confirmation)
     end
   end
-  
-  describe "password validations" do
 
+  describe "password validations" do
     it "should require a password" do
       Citygate::User.new(@attr.merge(:password => "", :password_confirmation => "")).
         should_not be_valid
@@ -85,21 +83,18 @@ describe Citygate::User do
       Citygate::User.new(@attr.merge(:password_confirmation => "invalid")).
         should_not be_valid
     end
-    
+
     it "should reject short passwords" do
       short = "a" * 5
       hash = @attr.merge(:password => short, :password_confirmation => short)
       Citygate::User.new(hash).should_not be_valid
     end
-    
   end
-  
+
   describe "password encryption" do
     subject { Citygate::User.create!(@attr) }
 
     it { should respond_to(:encrypted_password) }
     its(:encrypted_password) { should_not be_blank }
-
   end
-
 end
