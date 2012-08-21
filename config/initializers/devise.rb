89e68d274058fd162bac3c52de25c8a2d10f7e37
Zamith <zamith.28@gmail.com>
2012-08-21 14:46:05 +0100

    Graceful failure when facebook credentials are not present

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -205,7 +205,7 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', :scope => 'user,public_repo'
-  unless Rails.env.test? || !ACCOUNTS['facebook']
+  unless Rails.env.test? || !(ACCOUNTS && ACCOUNTS['facebook'])
     config.omniauth :facebook, ACCOUNTS['facebook']['app_id'], ACCOUNTS['facebook']['app_secret'],
       { :scope => 'email, offline_access' }
       
