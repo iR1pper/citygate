7691e5bb52988310c67ab8f15e2e8f50d781105e
Zamith <zamith.28@gmail.com>
2012-09-07 02:59:38 +0100

    Refactoring redundant code

--- app/assets/javascripts/citygate.js
+++ app/assets/javascripts/citygate.js
@@ -12,6 +12,6 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require ./citygate/jquery.pjax
-//= require ./citygate/admin_users
-//= require ./citygate/home
+//= require citygate/jquery.pjax
+//= require citygate/admin_users
+//= require citygate/home
