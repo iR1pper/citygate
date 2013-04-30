c6c76f4d92a0d2af70896e878637564fc04a2234
Zamith <zamith.28@gmail.com>
2013-04-30 16:56:12 +0100

    Actually not defining ACCOUNTS [ci skip]

--- config/initializers/01_load_config.rb
+++ config/initializers/01_load_config.rb
@@ -1,2 +1,2 @@
 file_name = "#{Rails.root}/config/accounts.yml"
-ACCOUNTS = YAML.load_file(file_name) unless Rails.env.test? or not File.exist?(file_name) or not defined?(ACCOUNTS)
+ACCOUNTS = YAML.load_file(file_name) unless Rails.env.test? or not File.exist?(file_name) or defined?(ACCOUNTS)
