3fe249de7bfcb1ceb4d0b198727f0e423611d43b
Miguel Palhas <mpalhas@gmail.com>
2013-06-17 14:02:32 +0100

    updated schema

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130308173926) do
+ActiveRecord::Schema.define(:version => 20130617124916) do
 
   create_table "citygate_authorizations", :force => true do |t|
     t.string   "provider"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(:version => 20130308173926) do
     t.string   "image_url"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.string   "email"
   end
 
   create_table "citygate_roles", :force => true do |t|
